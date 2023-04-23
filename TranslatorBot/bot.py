04fbcc2d967709993fba9b0bec00646ab1a6f776
settesss <nnmanakovv@gmail.com>
2023-04-23 23:32:37 +0700

    feature(bot.py): функционал по выводу и обновлению слов полностью обновлён и вынесен в функцию ask_question(chat_id), добавлен выход из процедуры изучения новых слов

--- TranslatorBot/bot.py
+++ TranslatorBot/bot.py
@@ -37,43 +37,38 @@ def handle_help(message):
     bot.delete_message(message.chat.id, message.message_id)
 
 
+def ask_question(chat_id):
+    global row_number, question_word, answer_word
+    row_number = str(random.randrange(1, SHEET_ROWS_COUNT))
+    question_word = sheet['A' + row_number].value
+    answer_word = sheet['B' + row_number].value
+    other_words = [sheet['B' + str(i)].value for i in range(1, SHEET_ROWS_COUNT) if
+                   i != row_number]
+    other_words_sampled = random.sample(other_words, 3)
+    answer_cells = [answer_word] + other_words_sampled
+    random.shuffle(answer_cells)
+    answer_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    answer_markup.row(answer_cells[0], answer_cells[1])
+    answer_markup.row(answer_cells[2], answer_cells[3])
+    answer_markup.row("Я устал ⛔")
+    bot.send_message(chat_id, question_word.title(), reply_markup=answer_markup)
+    del question_word
+
+
 @bot.message_handler(content_types=['text'])
 def echo(message):
-    global row_number, question_word, answer_word
     if message.text.lower() == 'изучать слова 📖':
         bot.send_message(message.chat.id, "Поехали!")
-        row_number = str(random.randrange(1, SHEET_ROWS_COUNT))
-        question_word = sheet['A' + row_number].value
-        answer_word = sheet['B' + row_number].value
-        answer_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-        answer_markup.row(sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value,
-                          sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value)
-        answer_markup.row(sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value,
-                          answer_word)
-        bot.send_message(message.chat.id, question_word.title(), reply_markup=answer_markup)
-    elif message.text.lower() == answer_word.lower():
+        ask_question(message.chat.id)
+    elif message.text.lower() == answer_word:
         bot.send_message(message.chat.id, "Молодец!")
-        row_number = str(random.randrange(1, SHEET_ROWS_COUNT))
-        question_word = sheet['A' + row_number].value
-        answer_word = sheet['B' + row_number].value
-        answer_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-        answer_markup.row(sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value,
-                          sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value)
-        answer_markup.row(sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value,
-                          answer_word)
-        bot.send_message(message.chat.id, question_word.title(), reply_markup=answer_markup)
+        ask_question(message.chat.id)
+    elif message.text.lower() == 'Я устал ⛔'.lower():
+        bot.send_message(message.chat.id, "Спасибо за завершение работы!")
+        handle_start(message)
     else:
         bot.send_message(message.chat.id, "Не молодец!")
-        row_number = str(random.randrange(1, SHEET_ROWS_COUNT))
-        question_word = sheet['A' + row_number].value
-        answer_word = sheet['B' + row_number].value
-        answer_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-        answer_markup.row(sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value,
-                          sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value)
-        answer_markup.row(sheet['B' + str(random.randrange(1, SHEET_ROWS_COUNT))].value,
-                          answer_word)
-        bot.send_message(message.chat.id, question_word.title(), reply_markup=answer_markup)
-    del question_word
+        ask_question(message.chat.id)
 
 
 bot.polling()
